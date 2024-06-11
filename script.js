6037d8da6eb308dde35c9d53b06a4e96a806398a
johnj-p <jojairop@gmail.com>
2024-06-11 12:51:44 -0500

    Menu hamburguesa

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function scrollFunction() {
     document.querySelector('.volver-arriba').style.display = "none";
   }
 }
+
+document.addEventListener('click', function(event) {
+  var checkbox = document.getElementById('menu-toggle');
+  var label = document.querySelector('label[for="menu-toggle"]');
+  var elemento = document.getElementById('menu');
+  
+  // Verificar si el clic ocurrió fuera del checkbox y de su etiqueta asociada
+  if (event.target !== checkbox && event.target !== label) {
+      checkbox.checked = false; // Desmarcar el checkbox
+  }
+  if (checkbox.checked == false){
+    elemento.style.display = 'none'; // Ocultar el elemento
+  } else {
+    elemento.style.display = 'block'; // Mostrar el elemento
+  }
+})
+
